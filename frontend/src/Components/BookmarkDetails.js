cadab1b422df44f9c4baf26b3626ff81fc4919b7
Mukayila ALAO <mukayilaalao@pursuit.org>
2022-02-03 12:15:18 -0500

    Added frontend view

--- frontend/src/Components/BookmarkDetails.js
+++ frontend/src/Components/BookmarkDetails.js
@@ -1,8 +1,54 @@
-// import { useState } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
 
+const API = process.env.REACT_APP_API_URL;
 function BookmarkDetails() {
-  // const [bookmark, setBookmark] = useState([]);
-  return <article></article>;
+  const [bookmark, setBookmark] = useState({});
+  const { id } = useParams();
+  const navigate = useNavigate();
+  useEffect(() => {
+    axios
+      .get(`${API}/bookmarks/${id}`)
+      .then((response) => setBookmark(response.data))
+      .catch((e) => console.log(e));
+  }, [id]);
+  const handleDelete = () => {
+    axios
+      .delete(`${API}/bookmarks/${id}`)
+      .then(() => navigate("/bookmarks"))
+      .catch((e) => console.log(e));
+  };
+  return (
+    <article>
+      <h3>
+        {true ? <span>⭐️</span> : null} {bookmark.name}
+      </h3>
+      <h5>
+        <span>
+          <a href={bookmark.url}>{bookmark.name}</a>
+        </span>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {bookmark.url}
+      </h5>
+      <h6>{bookmark.category}</h6>
+      <p>{bookmark.description}</p>
+      <div className="showNavigation">
+        <div>
+          <Link to={`/bookmarks`}>
+            <button>Back</button>
+          </Link>
+        </div>
+        <div>
+          <Link to={`/bookmarks/${id}/edit`}>
+            <button>Edit</button>
+          </Link>
+        </div>
+        <div>
+          <button onClick={handleDelete}>Delete</button>
+        </div>
+      </div>
+    </article>
+  );
 }
 
 export default BookmarkDetails;
